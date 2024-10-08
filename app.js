2760f5daa5e300cf8d22de08e0a542afb3537ea6
Sabino Hernandez Paulino <sakuraeyes21@gmail.com>
2024-10-08 11:24:55 -0600

    configuration and intro for TS

---
+++ app.js
@@ -0,0 +1,10 @@
+"use strict";
+const msg = "Hello World";
+console.log(msg);
+// msg = 123; // Error: Type '123' is not assignable to type 'string'.
+const hero = {
+    name: "Ironman",
+    age: 40,
+};
+// hero.age = "40";
+console.log(hero.age + 1);
---
+++ app.ts
@@ -0,0 +1,10 @@
+const msg: string = "Hello World";
+console.log(msg);
+// msg = 123; // Error: Type '123' is not assignable to type 'string'.
+
+const hero = {
+  name: "Ironman",
+  age: 40,
+};
+// hero.age = "40";
+console.log(hero.age + 1);
